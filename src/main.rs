c250b39dc91ac598e2e30d3f759c8a5d43b8d5fb
Andrew Yourtchenko <ayourtch@gmail.com>
2020-05-15 19:39:09 +0000

    A copy-paste from ssh2 docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    use ssh2::Session;
+    use std::io::prelude::*;
+    use std::net::TcpStream;
+
+    // Connect to the local SSH server
+    let tcp = TcpStream::connect("127.0.0.1:22").unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_agent("username").unwrap();
+
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec("ls").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    println!("{}", s);
+    channel.wait_close();
+    println!("{}", channel.exit_status().unwrap());
 }
